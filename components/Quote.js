84b9e044ce1be91da426b1a5e509ff3a0e698ace
Sihle Kambula <kambulasihle@gamil.com>
2022-07-09 16:15:13 +0200

    quote section done

--- components/Quote.js
+++ components/Quote.js
@@ -5,26 +5,44 @@ import styles from "../styles/Quote.module.css";
 export default function Quote() {
   return (
     <>
-      <h1>Get A Quote</h1>
       <div className={styles.container}>
         <Grid container>
           <Grid item md={6} className={styles.form}>
+            <h1 style={{ fontSize: "3rem", color: "white" }}>Get A Quote</h1>
             <div>
-              <TextField id='outlined-required' label='Full Name' />
+              <TextField
+                id='outlined-required'
+                label='Full Name'
+                variant='filled'
+                sx={{ backgroundColor: "white", borderRadius: "10px" }}
+                fullWidth
+              />
             </div>
             <br />
             <div>
-              <TextField id='outlined-required' label='Email' />
+              <TextField
+                id='outlined-required'
+                label='Email'
+                variant='filled'
+                sx={{ backgroundColor: "white", borderRadius: "10px" }}
+                fullWidth
+              />
             </div>
             <br />
             <div>
-              <TextField id='outlined-required' label='Comment' />
+              <TextField
+                id='outlined-required'
+                label='Comment'
+                variant='filled'
+                sx={{ backgroundColor: "white", borderRadius: "10px" }}
+                fullWidth
+              />
             </div>
-
+            <br />
             <Button>Submit</Button>
           </Grid>
           <Grid item md={6}>
-            <Image src='/images/quote.jpg' width={600} height={400} />
+            <Image src='/images/quote.jpg' width={600} height={500} />
           </Grid>
         </Grid>
       </div>
